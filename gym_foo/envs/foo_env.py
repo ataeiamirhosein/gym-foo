c205f83f20320fa650b7e84c10aae5470ad54163
Amirhosein Ataei <amirhosein.ataei@mail.polimi.it>
2020-10-05 18:48:48 +0200

    Update foo_env.py

--- gym_foo/envs/foo_env.py
+++ gym_foo/envs/foo_env.py
@@ -6,12 +6,12 @@ class FooEnv(gym.Env):
   metadata = {'render.modes': ['human']}
 
   def __init__(self):
-    ...
+    
   def step(self, action):
-    ...
+    
   def reset(self):
-    ...
+    
   def render(self, mode='human'):
-    ...
+    
   def close(self):
-    ...
+    
